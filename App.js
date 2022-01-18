1fe5984981c4d2392410fa72a3aacab9b0bc2937
raonix <raoni.gillet@gmail.com>
2022-01-18 14:59:55 +0100

    mountains background generation

--- App.js
+++ App.js
@@ -6,3 +6,30 @@ let app = Vue.createApp({
     }
 })
 app.mount('#app')
+
+const gui = new dat.GUI({ name: "My GUI" });
+const content = document.getElementsByClassName("content")[0];
+const scene = document.getElementById("scene");
+
+const config = {
+  size: 200,
+};
+
+gui
+  .add(config, "size", 20, 1920, 10)
+  .name("Size")
+  .onChange((newValue) => {
+    content.style.width = `${newValue}px`;
+    content.style.height = `${newValue}px`;
+  });
+
+  const setBg = () => {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor = "#" + randomColor;
+    color.innerHTML = "#" + randomColor;
+  }
+  
+  genNew.addEventListener("click", setBg);
+  setBg();
+
+  
