0455046334a121482d9a16efabd800757c8a88ce
raonix <raoni.gillet@gmail.com>
2022-01-18 16:16:50 +0100

    modifications

--- App.js
+++ App.js
@@ -32,4 +32,21 @@ gui
   genNew.addEventListener("click", setBg);
   setBg();
 
+  $("#rect")
+    .delay(500)
+	  .velocity({ x: "+=200", y: "25%" })
+    .velocity({ fillGreen: 255, strokeWidth: 2 })
+	  .velocity({ height: 50, width: 50 })
+    .velocity({ rotateZ: 90, scaleX: 0.5 })
+    .velocity("reverse", { delay: 250 });
+
+
+    $("#background").click(function() {
+      if ($(this).hasClass("active")) {
+        $("#background-scroll").show();
+      } else {
+        $("#background-scroll").hide();
+      }
+    });
+
   
